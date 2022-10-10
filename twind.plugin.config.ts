dda54e4404166701d4781d7e01cda4d1fd470864
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-10 17:30:48 +0200

    .

--- twind.plugin.config.ts
+++ twind.plugin.config.ts
@@ -1,8 +1,9 @@
 import { Options } from "$fresh/plugins/twind.ts";
+import { isProduction } from "./shared/config.ts";
 import TwindConfig from "./twind.config.ts";
 
 export default {
   selfURL: import.meta.url,
-  hash: false,
+  hash: isProduction,
   ...TwindConfig,
 } as Options;
