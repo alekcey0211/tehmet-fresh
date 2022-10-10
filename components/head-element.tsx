3105570020e1fc24a8f1ce39f7f6bfe24e0b2cbe
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-10 19:21:15 +0200

    .

--- components/head-element.tsx
+++ components/head-element.tsx
@@ -3,6 +3,176 @@ import { useContext } from "preact/hooks";
 import { Page } from "../context/page-context.tsx";
 import { isProduction } from "../shared/config.ts";
 
+const Metrika = () => {
+  return (
+    <>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function (d, w, c) {
+(w[c] = w[c] || []).push(function () {
+  try {
+    w.yaCounter32253134 = new Ya.Metrika({id: 32253134, clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true});
+  } catch (e) {}
+});
+
+var n = d.getElementsByTagName("script")[0],
+  s = d.createElement("script"),
+  f = function () {
+    n
+      .parentNode
+      .insertBefore(s, n);
+  };
+s.type = "text/javascript";
+s.async = true;
+s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+if (w.opera == "[object Opera]") {
+  d.addEventListener("DOMContentLoaded", f, false);
+} else {
+  f();
+}
+})(document, window, "yandex_metrika_callbacks");`,
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/32253134"
+            style="position:absolute; left:-9999px;"
+            alt=""
+          />
+        </div>
+      </noscript>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function (m, e, t, r, i, k, a) {
+m[i] = m[i] || function () {
+  (m[i].a = m[i].a || []).push(arguments)
+};
+m[i].l = 1 * new Date();
+k = e.createElement(t),
+a = e.getElementsByTagName(t)[0],
+k.async = 1,
+k.src = r,
+a
+  .parentNode
+  .insertBefore(k, a)
+})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(25681487, "init", {
+id: 25681487,
+clickmap: true,
+trackLinks: true,
+accurateTrackBounce: true,
+webvisor: true,
+trackHash: true
+});`,
+        }}
+      />
+      <meta
+        name="google-site-verification"
+        content="hnkUOKg3b9TYxrf06HD7rd4PPxEWycsnauhnAZ279Wo"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+(function (w, d, s, l, i) {
+w[l] = w[l] || [];
+w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+var f = d.getElementsByTagName(s)[0],
+
+  j = d.createElement(s),
+  dl = l != 'dataLayer'
+    ? '&l=' + l
+    : '';
+j.async = true;
+j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+f
+  .parentNode
+  .insertBefore(j, f);
+
+})(window, document, 'script', 'dataLayer', 'GTM-NZZRD7G');`,
+        }}
+      />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Z2FY1CPVCP"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+ window.dataLayer = window.dataLayer || [];
+ function gtag() {
+   dataLayer.push(arguments);
+ }
+ gtag('js', new Date());
+
+ gtag('config', 'G-Z2FY1CPVCP');`,
+        }}
+      />
+      <meta name="yandex-verification" content="f3d851eb6216d1c3" />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/25681487"
+            style="position:absolute; left:-9999px;"
+            alt=""
+          />
+        </div>
+      </noscript>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NZZRD7G"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+ (function (w, d, n, c) {
+w.CalltouchDataObject = n;
+w[n] = function () {
+  w[n]["callbacks"].push(arguments)
+};
+if (!w[n]["callbacks"]) {
+  w[n]["callbacks"] = []
+}
+w[n]["loaded"] = false;
+if (typeof c !== "object") {
+  c = [c]
+}
+w[n]["counters"] = c;
+for (var i = 0; i < c.length; i += 1) {
+  p(c[i])
+}
+function p(cId) {
+  var a = d.getElementsByTagName("script")[0],
+    s = d.createElement("script"),
+    i = function () {
+      a
+        .parentNode
+        .insertBefore(s, a)
+    };
+  s.type = "text/javascript";
+  s.async = true;
+  s.src = "https://mod.calltouch.ru/init.js?id=" + cId;
+  if (w.opera == "[object Opera]") {
+    d.addEventListener("DOMContentLoaded", i, false)
+  } else {
+    i()
+  }
+}
+})(window, document, "ct", "sv8t3ldn");`,
+        }}
+      />
+    </>
+  );
+};
+
 export function HeadElement({
   description = "Группа Компаний Техмет - ведущий поставщик в России в области комплексных поставок продукции для монтажа нефтегазопроводов Цель компании создавать уверенность у потребителей в бесперебойности производства",
   image = "logo-color.png",
@@ -25,173 +195,7 @@ export function HeadElement({
         content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover"
       /> */}
 
-      {isProduction && (
-        <>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-		 (function (d, w, c) {
-		  (w[c] = w[c] || []).push(function () {
-			  try {
-				  w.yaCounter32253134 = new Ya.Metrika({id: 32253134, clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true});
-			  } catch (e) {}
-		  });
-  
-		  var n = d.getElementsByTagName("script")[0],
-			  s = d.createElement("script"),
-			  f = function () {
-				  n
-					  .parentNode
-					  .insertBefore(s, n);
-			  };
-		  s.type = "text/javascript";
-		  s.async = true;
-		  s.src = "https://mc.yandex.ru/metrika/watch.js";
-  
-		  if (w.opera == "[object Opera]") {
-			  d.addEventListener("DOMContentLoaded", f, false);
-		  } else {
-			  f();
-		  }
-	  })(document, window, "yandex_metrika_callbacks");`,
-            }}
-          />
-          <noscript>
-            <div>
-              <img
-                src="https://mc.yandex.ru/watch/32253134"
-                style="position:absolute; left:-9999px;"
-                alt=""
-              />
-            </div>
-          </noscript>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-		  (function (m, e, t, r, i, k, a) {
-			m[i] = m[i] || function () {
-				(m[i].a = m[i].a || []).push(arguments)
-			};
-			m[i].l = 1 * new Date();
-			k = e.createElement(t),
-			a = e.getElementsByTagName(t)[0],
-			k.async = 1,
-			k.src = r,
-			a
-				.parentNode
-				.insertBefore(k, a)
-		})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-  
-		ym(25681487, "init", {
-			id: 25681487,
-			clickmap: true,
-			trackLinks: true,
-			accurateTrackBounce: true,
-			webvisor: true,
-			trackHash: true
-		});`,
-            }}
-          />
-          <meta
-            name="google-site-verification"
-            content="hnkUOKg3b9TYxrf06HD7rd4PPxEWycsnauhnAZ279Wo"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-		  (function (w, d, s, l, i) {
-			w[l] = w[l] || [];
-			w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
-			var f = d.getElementsByTagName(s)[0],
-  
-				j = d.createElement(s),
-				dl = l != 'dataLayer'
-					? '&l=' + l
-					: '';
-			j.async = true;
-			j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-			f
-				.parentNode
-				.insertBefore(j, f);
-  
-		})(window, document, 'script', 'dataLayer', 'GTM-NZZRD7G');`,
-            }}
-          />
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-Z2FY1CPVCP"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-		   window.dataLayer = window.dataLayer || [];
-		   function gtag() {
-			   dataLayer.push(arguments);
-		   }
-		   gtag('js', new Date());
-	 
-		   gtag('config', 'G-Z2FY1CPVCP');`,
-            }}
-          />
-          <meta name="yandex-verification" content="f3d851eb6216d1c3" />
-          <noscript>
-            <div>
-              <img
-                src="https://mc.yandex.ru/watch/25681487"
-                style="position:absolute; left:-9999px;"
-                alt=""
-              />
-            </div>
-          </noscript>
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-NZZRD7G"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-		   (function (w, d, n, c) {
-			w.CalltouchDataObject = n;
-			w[n] = function () {
-				w[n]["callbacks"].push(arguments)
-			};
-			if (!w[n]["callbacks"]) {
-				w[n]["callbacks"] = []
-			}
-			w[n]["loaded"] = false;
-			if (typeof c !== "object") {
-				c = [c]
-			}
-			w[n]["counters"] = c;
-			for (var i = 0; i < c.length; i += 1) {
-				p(c[i])
-			}
-			function p(cId) {
-				var a = d.getElementsByTagName("script")[0],
-					s = d.createElement("script"),
-					i = function () {
-						a
-							.parentNode
-							.insertBefore(s, a)
-					};
-				s.type = "text/javascript";
-				s.async = true;
-				s.src = "https://mod.calltouch.ru/init.js?id=" + cId;
-				if (w.opera == "[object Opera]") {
-					d.addEventListener("DOMContentLoaded", i, false)
-				} else {
-					i()
-				}
-			}
-		})(window, document, "ct", "sv8t3ldn");`,
-            }}
-          />
-        </>
-      )}
+      {isProduction && <Metrika />}
 
       {/* Facebook Meta Tags */}
       <meta property="og:url" content={url.href} />
@@ -258,6 +262,24 @@ export function HeadElement({
         ></script>
       )}
       <script type="module" src="/script.js" defer></script>
+      {isProduction && (
+        <script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function (w, d, u) {
+        var s = d.createElement('script');
+        s.async = true;
+        s.src = u + '?' + (
+        Date.now() / 60000 | 0);
+        var h = d.getElementsByTagName('script')[0];
+        h
+          .parentNode
+          .insertBefore(s, h);
+      })(window, document, 'https://crm.tehmet.su/upload/crm/site_button/loader_1_5bqfwf.js');`,
+          }}
+        />
+      )}
     </Head>
   );
 }
