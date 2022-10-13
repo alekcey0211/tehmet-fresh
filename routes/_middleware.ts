ecaf3044cfdc71f0332185ff82f9340985e6d516
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-13 08:28:29 +0200

    .

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,9 +1,9 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { isProduction } from "../shared/config.ts";
 
-export async function handler(req: Request, ctx: MiddlewareHandlerContext) {
+export function handler(req: Request, ctx: MiddlewareHandlerContext) {
   if (isProduction && req.url.startsWith("http://")) {
     return Response.redirect(req.url.replace("http://", "https://"), 301);
   }
-  return await ctx.next();
+  return ctx.next();
 }
