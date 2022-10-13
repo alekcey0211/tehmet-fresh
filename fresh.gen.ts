60b37328fc85eb5b893cd144ec36b0f784798661
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-13 08:52:05 +0200

    .

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,28 +4,27 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_middleware.ts";
-import * as $2 from "./routes/about/career/index.tsx";
-import * as $3 from "./routes/about/career/vacancy/buhgalter.tsx";
-import * as $4 from "./routes/about/career/vacancy/menedzher-po-snabzheniyu.tsx";
-import * as $5 from "./routes/about/career/vacancy/menedzher.tsx";
-import * as $6 from "./routes/about/career/vacancy/operator-pk.tsx";
-import * as $7 from "./routes/about/defaulter.tsx";
-import * as $8 from "./routes/about/index.tsx";
-import * as $9 from "./routes/about/team.tsx";
-import * as $10 from "./routes/blog.tsx";
-import * as $11 from "./routes/brands/[url].tsx";
-import * as $12 from "./routes/catalog/[url].tsx";
-import * as $13 from "./routes/catalog/index.tsx";
-import * as $14 from "./routes/contacts.tsx";
-import * as $15 from "./routes/data/popular-products.ts";
-import * as $16 from "./routes/data/product.ts";
-import * as $17 from "./routes/data/products-page.ts";
-import * as $18 from "./routes/data/products-search.ts";
-import * as $19 from "./routes/delivery.tsx";
-import * as $20 from "./routes/index.tsx";
-import * as $21 from "./routes/products/[url].tsx";
-import * as $22 from "./routes/video.tsx";
+import * as $1 from "./routes/about/career/index.tsx";
+import * as $2 from "./routes/about/career/vacancy/buhgalter.tsx";
+import * as $3 from "./routes/about/career/vacancy/menedzher-po-snabzheniyu.tsx";
+import * as $4 from "./routes/about/career/vacancy/menedzher.tsx";
+import * as $5 from "./routes/about/career/vacancy/operator-pk.tsx";
+import * as $6 from "./routes/about/defaulter.tsx";
+import * as $7 from "./routes/about/index.tsx";
+import * as $8 from "./routes/about/team.tsx";
+import * as $9 from "./routes/blog.tsx";
+import * as $10 from "./routes/brands/[url].tsx";
+import * as $11 from "./routes/catalog/[url].tsx";
+import * as $12 from "./routes/catalog/index.tsx";
+import * as $13 from "./routes/contacts.tsx";
+import * as $14 from "./routes/data/popular-products.ts";
+import * as $15 from "./routes/data/product.ts";
+import * as $16 from "./routes/data/products-page.ts";
+import * as $17 from "./routes/data/products-search.ts";
+import * as $18 from "./routes/delivery.tsx";
+import * as $19 from "./routes/index.tsx";
+import * as $20 from "./routes/products/[url].tsx";
+import * as $21 from "./routes/video.tsx";
 import * as $$0 from "./islands/BrandsFooterList.tsx";
 import * as $$1 from "./islands/CatalogItem.tsx";
 import * as $$2 from "./islands/CatalogList.tsx";
@@ -46,28 +45,27 @@ import * as $$15 from "./islands/TopNewSlider.tsx";
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/_middleware.ts": $1,
-    "./routes/about/career/index.tsx": $2,
-    "./routes/about/career/vacancy/buhgalter.tsx": $3,
-    "./routes/about/career/vacancy/menedzher-po-snabzheniyu.tsx": $4,
-    "./routes/about/career/vacancy/menedzher.tsx": $5,
-    "./routes/about/career/vacancy/operator-pk.tsx": $6,
-    "./routes/about/defaulter.tsx": $7,
-    "./routes/about/index.tsx": $8,
-    "./routes/about/team.tsx": $9,
-    "./routes/blog.tsx": $10,
-    "./routes/brands/[url].tsx": $11,
-    "./routes/catalog/[url].tsx": $12,
-    "./routes/catalog/index.tsx": $13,
-    "./routes/contacts.tsx": $14,
-    "./routes/data/popular-products.ts": $15,
-    "./routes/data/product.ts": $16,
-    "./routes/data/products-page.ts": $17,
-    "./routes/data/products-search.ts": $18,
-    "./routes/delivery.tsx": $19,
-    "./routes/index.tsx": $20,
-    "./routes/products/[url].tsx": $21,
-    "./routes/video.tsx": $22,
+    "./routes/about/career/index.tsx": $1,
+    "./routes/about/career/vacancy/buhgalter.tsx": $2,
+    "./routes/about/career/vacancy/menedzher-po-snabzheniyu.tsx": $3,
+    "./routes/about/career/vacancy/menedzher.tsx": $4,
+    "./routes/about/career/vacancy/operator-pk.tsx": $5,
+    "./routes/about/defaulter.tsx": $6,
+    "./routes/about/index.tsx": $7,
+    "./routes/about/team.tsx": $8,
+    "./routes/blog.tsx": $9,
+    "./routes/brands/[url].tsx": $10,
+    "./routes/catalog/[url].tsx": $11,
+    "./routes/catalog/index.tsx": $12,
+    "./routes/contacts.tsx": $13,
+    "./routes/data/popular-products.ts": $14,
+    "./routes/data/product.ts": $15,
+    "./routes/data/products-page.ts": $16,
+    "./routes/data/products-search.ts": $17,
+    "./routes/delivery.tsx": $18,
+    "./routes/index.tsx": $19,
+    "./routes/products/[url].tsx": $20,
+    "./routes/video.tsx": $21,
   },
   islands: {
     "./islands/BrandsFooterList.tsx": $$0,
