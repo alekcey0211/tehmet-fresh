7b11e4b20bc372e0e759b02dca4cd43af5ae48c1
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-19 15:04:42 +0200

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
       script: "./main.ts",
       interpreter: "/root/.deno/bin/deno",
       interpreter_args: "run -A --unstable",
-      watch: true,
-      watch_delay: 1000,
-      ignore_watch: ["db"],
+      // watch: true,
+      // watch_delay: 1000,
+      // ignore_watch: ["db"],
     },
   ],
 };
