1c3b9604070e9f38eb85dd7d0e9fe6ea0f14be19
Aleksei Ezhov <alekcey-ezh@yandex-team.ru>
2022-10-19 15:00:23 +0200

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,8 @@ module.exports = {
   apps: [
     {
       name: "tehmet",
-      script: "./main.ts",
-      interpreter: "/root/.deno/bin/deno",
-      interpreter_args: "run -A --unstable",
-      watch: true,
-      watch_delay: 1000,
-      ignore_watch: ["db"],
+      script: "start",
+      interpreter: "/root/.deno/bin/denon",
     },
   ],
 };
